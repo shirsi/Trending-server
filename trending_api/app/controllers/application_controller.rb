bf1443c963d8acd08c557cdd3a584dbfac221064
Safia Hirsi <safiahirsi@Safias-MacBook-Pro.local>
2020-04-03 12:03:28 -0500

    pre -kill

--- trending_api/app/controllers/application_controller.rb
+++ trending_api/app/controllers/application_controller.rb
@@ -1,22 +1,23 @@
-class ApplicationController < ActionController::API
-    def authenticate_token
-        puts "AUTHENTICATE JWT"
-        render json: { status: 401, message: 'Unauthorized' } unless decode_token(bearer_token)
-      end
-    
-      def bearer_token
-        puts "BEARER TOKEN"
-        puts header = request.env["HTTP_AUTHORIZATION"]
+class ApplicationController < ActionController::API                                
+                                                                                   
+    def authenticate_token                                                      
+      render json: { status: 401, message: 'Unauthorized' } unless decode_token(bearer_token)
+    end                                                                                                                                                            
+    def bearer_token                                                                 
+      header = request.env['HTTP_AUTHORIZATION']                                     
+      pattern = /^Bearer /                                                           
+      header.gsub(pattern, '') if header && header.match(pattern)                    
+    end                                                                              
+                                                                                     
+    def decode_token(token_input)                                                    
+      JWT.decode(token_input, ENV['JWT_SECRET'], true)                                           
+        rescue                                                                           
+      render json: { status: 401, message: 'Unauthorized' }                          
+        end    
+    end                                                           
+  def get_current_user  
 
-        pattern =/^Bearer /
-        puts "TOKEN WITHOUT BEARER"
-        puts header.gsub(pattern, '') if header && header.match(pattern)
-        header.gsub(pattern, '') if header && header.match(pattern)
-      end
-    
-      def decode_token(token_input)
-        puts 'DECODE TOKEN, token input: #(token_input}'
-        puts token = JWT.decode(token_input, ENV['JWT_SECRET'], true, { :algorithm => 'HS256' })
-        render json:{decoded: token}
-      end
-end
+    return if !bearer_token                                                      
+    decoded_jwt = decode_token(bearer_token)                                     
+    User.find(decoded_jwt[0]["user"]["id"])                                                     
+  end                                                                                           
