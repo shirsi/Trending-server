f99b63ddb69939b15598857edb6b59c2d724f6ca
Alfonso Garcia <ajgarcia321@gmail.com>
2020-04-03 08:45:13 -0700

    ENV Completed

--- trending_api/app/controllers/users_controller.rb
+++ trending_api/app/controllers/users_controller.rb
@@ -1,60 +1,76 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+    before_action :set_user, only: [:show, :update, :destroy]
 
-  # GET /users
-  def index
-    @users = User.all
+    # GET /users
+    def index
+        @users = User.all
 
-    render json: @users
-  end
-
-  # GET /users/1
-  def show
-    render json: @user
-  end
-
-  # POST /users
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
+        render json: @users
     end
-  end
 
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
+    # GET /users/1
+    def show
+        render json: @user
     end
-  end
 
-  # DELETE /users/1
-  def destroy
-    @user.destroy
-  end
+    # POST /users
+    def create
+        @user = User.new(user_params)
 
-  def login
-    user = User.find_by(username: params[:user][:username])
+        if @user.save
+            render json: @user, status: :created, location: @user
+        else
+            render json: @user.errors, status: :unprocessable_entity
+        end
+    end
+
+    # PATCH/PUT /users/1
+    def update
+        if @user.update(user_params)
+            render json: @user
+        else
+            render json: @user.errors, status: :unprocessable_entity
+        end
+    end
+
+    # DELETE /users/1
+    def destroy
+        @user.destroy
+    end
+
+    def login
+        user = User.find_by(username: params[:user][:username])
         if user && user.authenticate(params[:user][:password])
             render json: {status: 200, user: user}
         else
             render json: {status: 401, message: "Unauthorized"}
         end
-  end
+    end
 
-  private
+    private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+        @user = User.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:username, :password_digest)
+        params.require(:user).permit(:username, :password_digest)
+    end
+
+    def create_token(id, username)
+        JWT.encode(payload(id, username), ENV['JWT_SECRET'], 'HS256')
+    end
+
+    def payload(id, username)
+        {
+            exp: (Time.now + 30.minutes).to_i,
+            iat: Time.now.to_i,
+            iss: ENV['JWT_ISSUER'],
+            user: {
+                id: id,
+                username: username
+            }
+        }
     end
 end
