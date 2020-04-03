c5d40a8b885cdeb776cfa43f8a974f5c51bbc7d1
Safia Hirsi <safiahirsi@Safias-MacBook-Pro.local>
2020-04-03 11:51:30 -0500

    decode token setup

--- trending_api/app/controllers/application_controller.rb
+++ trending_api/app/controllers/application_controller.rb
@@ -7,9 +7,16 @@ class ApplicationController < ActionController::API
       def bearer_token
         puts "BEARER TOKEN"
         puts header = request.env["HTTP_AUTHORIZATION"]
+
+        pattern =/^Bearer /
+        puts "TOKEN WITHOUT BEARER"
+        puts header.gsub(pattern, '') if header && header.match(pattern)
+        header.gsub(pattern, '') if header && header.match(pattern)
       end
     
       def decode_token(token_input)
-        puts "DECODE TOKEN"
+        puts 'DECODE TOKEN, token input: #(token_input}'
+        puts token = JWT.decode(token_input, ENV['JWT_SECRET'], true, { :algorithm => 'HS256' })
+        render json:{decoded: token}
       end
 end
