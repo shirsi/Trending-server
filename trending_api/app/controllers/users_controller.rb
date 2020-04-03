3473d37aff9928c228564ecdda5a67f0b62610af
Alfonso Garcia <ajgarcia321@gmail.com>
2020-04-03 08:38:55 -0700

    Fixed local rails version, added jwt & dotenv, CURL request for new user

--- trending_api/app/controllers/users_controller.rb
+++ trending_api/app/controllers/users_controller.rb
@@ -38,6 +38,15 @@ class UsersController < ApplicationController
     @user.destroy
   end
 
+  def login
+    user = User.find_by(username: params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+            render json: {status: 200, user: user}
+        else
+            render json: {status: 401, message: "Unauthorized"}
+        end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
